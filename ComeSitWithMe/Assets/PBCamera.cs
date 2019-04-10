bf2d37f5585b794551ac7c40b6471cd062d1a1b6
Gavin Wood <gavinneilwood@gmail.com>
2019-04-10 13:18:38 +0100

    Post hackathon commit with cleaned up assets to ensure compilation on others machines. Code as-was in the jam.

---
+++ ComeSitWithMe/Assets/PBCamera.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PBCamera : MonoBehaviour
+{
+    public float minX = -360.0f;
+    public float maxX = 360.0f;
+
+    public float minY = -45.0f;
+    public float maxY = 45.0f;
+
+    public float sensX = 100.0f;
+    public float sensY = 100.0f;
+
+    float rotationY = 0.0f;
+    float rotationX = 0.0f;
+
+    float MouseX;
+    float MouseY;
+
+    public VideoPlayer videoPlayer;
+    public Canvas canvas;
+
+    void Start()
+    {
+        BWEventManager.StartListening("ResetVR", ResetVR);
+
+        // Reset the VR
+        BWEventManager.TriggerEvent("ResetVR");
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // ResetVR
+
+    void ResetVR()
+    {
+        Debug.Log("ResetVR");
+        OVRManager.display.RecenterPose();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            BWEventManager.TriggerEvent("Stop");
+            videoPlayer.Pause();
+            canvas.gameObject.SetActive(true);            // Show the menu
+        }
+
+        // Reset all the VR - so recalibrate pose
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            BWEventManager.TriggerEvent("ResetVR");
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            var x = Input.GetAxis("Mouse X");
+            var y = Input.GetAxis("Mouse Y");
+            if (x != MouseX || y != MouseY)
+            {
+                rotationX += x * sensX * Time.deltaTime;
+                rotationY += y * sensY * Time.deltaTime;
+                rotationY = Mathf.Clamp(rotationY, minY, maxY);
+                MouseX = x;
+                MouseY = y;
+                transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+            }
+        }
+    }
+}
---
+++ ComeSitWithMe/Assets/PBMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class PBMenu : MonoBehaviour
+{
+    public Button francis;
+    public VideoPlayer videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.Pause();
+        francis.onClick.AddListener(ButtonClicked);
+    }
+
+    void ButtonClicked()
+    {
+        videoPlayer.Play();
+        Debug.Log("Beach");
+        gameObject.SetActive(false);                // Hide the menu
+        BWEventManager.TriggerEvent("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
